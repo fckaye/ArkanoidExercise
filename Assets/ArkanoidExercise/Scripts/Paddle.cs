9e3be8118cfff2b0b51b8db688fb4a9b945936e5
Fernando Caye <fdcaye@gmail.com>
2022-02-18 22:12:02 +0900

    Add paddle movement: will move to direction swiped to.

--- Assets/ArkanoidExercise/Scripts/Paddle.cs
+++ Assets/ArkanoidExercise/Scripts/Paddle.cs
@@ -2,17 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paddle : MonoBehaviour
+namespace ArkanoidExercise.Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Paddle : MonoBehaviour
     {
-        
-    }
+        #region SerializedFields
+        [SerializeField] private float speed;
+        #endregion // SerializedFields
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        #region Class Members
+        private Vector2 dragStart;
+        private Vector2 dragEnd;
+        #endregion // Class Members
+
+        #region MonoBehaviour Callbacks
+        private void Update()
+        {
+            CheckInput();
+            MovePaddle();
+        }
+        #endregion // MonoBehaviour Callbacks.
+
+        #region Private
+        private void CheckInput()
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    dragStart = touch.position;
+                    dragEnd = touch.position;
+                }
+
+                if (touch.phase == TouchPhase.Moved ||
+                    touch.phase == TouchPhase.Stationary)
+                {
+                    dragEnd = touch.position;
+                }
+
+                if (touch.phase == TouchPhase.Canceled ||
+                    touch.phase == TouchPhase.Ended)
+                {
+                    dragStart = Vector2.zero;
+                    dragEnd = Vector2.zero;
+                }
+            }
+        }
+
+        private void MovePaddle()
+        {
+            float x = GetSwipeDirection();
+            Vector3 direction = new Vector3(x, 0, 0);
+            this.transform.Translate(direction * speed * Time.deltaTime);
+        }
+
+        private float GetSwipeDirection()
+        {
+            float direction = dragEnd.x - dragStart.x;
+            return Mathf.Clamp(direction, -1.0f, 1.0f);
+        }
+        #endregion // Private
     }
 }
