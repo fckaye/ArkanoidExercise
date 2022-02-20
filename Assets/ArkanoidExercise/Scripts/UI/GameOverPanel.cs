3c9fe6fbec83a7cde1701f0820d5bee9cc73222f
Fernando Caye <fdcaye@gmail.com>
2022-02-20 11:20:54 +0900

    Add GameOverPanel. option to restart or go to menu at the end of the game.

---
+++ Assets/ArkanoidExercise/Scripts/UI/GameOverPanel.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace ArkanoidExercise.Scripts.UI
+{
+    public class GameOverPanel : MonoBehaviour
+    {
+        #region SerializedFields
+        [Header("Objects references.")]
+        [SerializeField] RectTransform layoutContainer;
+        [SerializeField] TextMeshProUGUI gameOverLabel;
+        [SerializeField] Button restartButton;
+        [SerializeField] Button mainMenuButton;
+
+        [Space(10)]
+        [Header("Config options.")]
+        [SerializeField] string loseMessage;
+        [SerializeField] string winMessage;
+        #endregion // SerializedFields
+
+        #region MonoBehaviour
+        private void Awake()
+        {
+            Initialize();
+        }
+        #endregion // MonoBehaviour
+
+        #region Public
+        public void Show(bool playerWin)
+        {
+            ShowLayout(true);
+
+            gameOverLabel.text = playerWin ? winMessage : loseMessage;
+        }
+        #endregion // Public
+
+        #region Private
+        private void Initialize()
+        {
+            ShowLayout(false);
+            gameOverLabel.text = string.Empty;
+            restartButton.onClick.AddListener(RestartCurrentLevel);
+            mainMenuButton.onClick.AddListener(GoToMainMenu);
+        }
+
+        private void ShowLayout(bool show)
+        {
+            if (layoutContainer is null) return;
+
+            layoutContainer.gameObject.SetActive(show);
+        }
+
+        private void RestartCurrentLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        private void GoToMainMenu()
+        {
+
+        }
+        #endregion // Private
+    }
+}
